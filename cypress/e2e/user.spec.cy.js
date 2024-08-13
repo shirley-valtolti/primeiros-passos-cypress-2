c4f1d21b116532612657020b9a351620add14006
Shirley da Silva Valtolti <shirleyvaltolti@gmail.com>
2024-08-13 00:35:20 -0300

    Add page objects

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,33 +1,35 @@
 import userData from '../fixtures/userData.json';
+import LoginPage from '../pages/loginPage.js'
+import DashboardPage from '../pages/dashboardPage.js';
+import MenuPage from '../pages/menuPage.js';
+
+const loginPage = new LoginPage()
+const dashboardPage = new DashboardPage()
+const menuPage = new MenuPage()
 
 describe('Orange HRM Tests', () => {
 
   const selectorList = {
-    usernameField: "[name='username']",
-    passwordField: "[name='password']",
-    loginButton: "[type='submit']",
-    sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
-    dashboardGrid: ".orangehrm-dashboard-grid",
-    wrongCredentialAlert: "[role='alert']",
-    myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
+    
     firstNameField: "[name='firstName']",
     lastNameField: "[name='lastName']",
     genericField: ".oxd-input--active",
     dateField: "[placeholder='yyyy-dd-mm']",
+    genericComboBox: ".oxd-select-text-input",
+    secondItemComboBox: ".oxd-select-dropdown > :nth-child(2) > span",
+    thirdItemComboBox: ":nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-select-wrapper > .oxd-select-text > .oxd-select-text--after > .oxd-icon",
     dateCloseButton: ".--close",
     submitButton: "[type='submit']",
   }
 
   
   it.only(' User Info Update - Sucess', () => {
+    loginPage.acessLoginPage()
+    loginPage.loginWithUser(userData.userSucess.username, userData.userSucess.password)
+    
+    dashboardPage.checkDashboardPage()
+    menuPage.accessMyInfo()
 
-    cy.visit('/auth/login')
-    cy.get(selectorList.usernameField).type(userData.userSucess.username)
-    cy.get(selectorList.passwordField).type(userData.userSucess.password)
-    cy.get(selectorList.loginButton).click()
-    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorList.dashboardGrid)
-    cy.get(selectorList.myInfoButton).click()
     cy.get(selectorList.firstNameField).clear().type('FirstNameTest')
     cy.get(selectorList.lastNameField).clear().type('LastNameTest')
     cy.get(selectorList.genericField).eq(3).clear().type('EmployeeId')
@@ -38,6 +40,11 @@ describe('Orange HRM Tests', () => {
     cy.get(selectorList.submitButton).eq(0).click()
     cy.get('body').should('contain', 'Successfully Updated')
     cy.get('.oxd-toast-close')
+
+    cy.get(selectorList.genericComboBox).eq(0).click()
+    cy.get(selectorList.secondItemComboBox).click()
+    cy.get(selectorList.genericComboBox).eq(1).click()
+    cy.get(selectorList.thirdItemComboBox).click()
   })
 
   it('Fail - Sucess', () => {
